4ff83f77876e27f68fc5b4f86570a287806f4597
tsubasa-fukuda <forkeeping.77@icloud.com>
2022-05-04 11:44:26 +0900

    Force Cast Violation修正

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -61,8 +61,9 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! ViewController2
-            dtl.vc1 = self
+            if let dtl = segue.destination as? ViewController2 {
+                dtl.vc1 = self
+            }
         }
 
     }
