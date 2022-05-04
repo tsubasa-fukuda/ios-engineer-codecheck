0823b3607268fca8af882af599a64b78e66e456b
tsubasa-fukuda <forkeeping.77@icloud.com>
2022-05-04 15:50:43 +0900

    変数名を変更

--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController2: UIViewController {
 
-    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var avatarView: UIImageView!
 
-    @IBOutlet weak var ttlLbl: UILabel!
+    @IBOutlet weak var fullNameLbl: UILabel!
 
-    @IBOutlet weak var langLbl: UILabel!
+    @IBOutlet weak var languageLbl: UILabel!
 
-    @IBOutlet weak var strsLbl: UILabel!
-    @IBOutlet weak var wchsLbl: UILabel!
-    @IBOutlet weak var frksLbl: UILabel!
-    @IBOutlet weak var isssLbl: UILabel!
+    @IBOutlet weak var stargazersCountLbl: UILabel!
+    @IBOutlet weak var watchersCountLbl: UILabel!
+    @IBOutlet weak var forksCountLbl: UILabel!
+    @IBOutlet weak var openIssuesCountLbl: UILabel!
 
     var vc1: ViewController!
 
@@ -28,11 +28,11 @@ class ViewController2: UIViewController {
 
         let repo = vc1.repos[vc1.idx]
 
-        langLbl.text = "Written in \(repo["language"] as? String ?? "")"
-        strsLbl.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
-        wchsLbl.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
-        frksLbl.text = "\(repo["forks_count"] as? Int ?? 0) forks"
-        isssLbl.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+        languageLbl.text = "Written in \(repo["language"] as? String ?? "")"
+        stargazersCountLbl.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        watchersCountLbl.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        forksCountLbl.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        openIssuesCountLbl.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
 
     }
@@ -41,14 +41,14 @@ class ViewController2: UIViewController {
 
         let repo = vc1.repos[vc1.idx]
 
-        ttlLbl.text = repo["full_name"] as? String
+        fullNameLbl.text = repo["full_name"] as? String
 
         if let owner = repo["owner"] as? [String: Any] {
-            if let imgURL = owner["avatar_url"] as? String {
-                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, _, _) in
-                    let img = UIImage(data: data!)!
+            if let avatarImgUrl = owner["avatar_url"] as? String {
+                URLSession.shared.dataTask(with: URL(string: avatarImgUrl)!) { (data, _, _) in
+                    let avatarImg = UIImage(data: data!)!
                     DispatchQueue.main.async {
-                        self.imgView.image = img
+                        self.avatarView.image = avatarImg
                     }
                 }.resume()
             }
